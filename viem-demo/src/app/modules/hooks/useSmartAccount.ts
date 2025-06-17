81046ffd4bf099d5c10807aa142732e08c347475
mathieuLARUELLE <math.laruelle@gmail.com>
2025-06-17 12:45:56 +0200

    residentKey discouraged - userVerification discouraged

--- viem-demo/src/app/modules/hooks/useSmartAccount.ts
+++ viem-demo/src/app/modules/hooks/useSmartAccount.ts
@@ -78,7 +78,7 @@ export function useSmartAccount() {
                 webAuthnOptions: {
                 authenticatorSelection: {
                 authenticatorAttachment: undefined, //coinbase
-                residentKey: "preferred", //required
+                residentKey: "discouraged", //required
                 userVerification: "discouraged", //coinbase
                 },
                 } as WebAuthnOptions,
